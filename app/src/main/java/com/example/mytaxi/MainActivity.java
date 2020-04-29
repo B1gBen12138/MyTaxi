617db4dd50f26db6640b172563282e664b3d3e1d
B1gBen12138 <x>
2020-04-29 17:49:15 +0800

    initPage finished

--- app/src/main/java/com/example/mytaxi/MainActivity.java
+++ app/src/main/java/com/example/mytaxi/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mytaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            final AnimatedVectorDrawable anim1 = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.anim);
+            final ImageView img1 = (ImageView) findViewById(R.id.logo);
+            img1.setImageDrawable(anim1);
+            anim1.start();
+        }
     }
 }
